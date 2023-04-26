0735b1a26f2a1824a14238a9bc927aedad9a1fb8
Joey Yu <joey@itsjoeoui.com>
2023-04-26 10:06:53 -0400

    Better blaster

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Report
+
+## Usage
+
+`python3 blaster.py [type: str] [num_strings: int] [string_length: int]`
--- blaster.py
+++ blaster.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import asyncio
 import httpx
@@ -5,11 +6,13 @@ from tqdm import tqdm
 
 NUM_REQUESTS = 1000  # Number of requests to send
 CONCURRENCY = 10  # Number of requests to send concurrently
-URL = "https://winter2023-comp598-group06-02.cs.mcgill.ca/light/sort/200/200/"  # The endpoint to send requests to
+# URL = "https://winter2023-comp598-group06-02.cs.mcgill.ca/light/sort/200/200/"  # The endpoint to send requests to
+# URL = "https://winter2023-comp598-group06-02.cs.mcgill.ca/medium/sort/200/200/"  # The endpoint to send requests to
+# URL = "https://winter2023-comp598-group06-02.cs.mcgill.ca/heavy/sort/100/500/"  # The endpoint to send requests to
 timeout = httpx.Timeout(60.0)
 
 
-async def send_request():
+async def send_request(URL: str):
     async with httpx.AsyncClient(timeout=timeout) as client:
         start_time = time.monotonic()
         code = 0
@@ -22,12 +25,12 @@ async def send_request():
         return code, end_time - start_time
 
 
-async def send_requests():
+async def send_requests(URL: str):
     start_time = time.monotonic()
     tasks = []
     latencies = []
     for _ in tqdm(range(NUM_REQUESTS), desc="Sending requests"):
-        task = asyncio.ensure_future(send_request())
+        task = asyncio.ensure_future(send_request(URL))
         task.add_done_callback(lambda t: latencies.append(t.result()[1]))
         tasks.append(task)
         if len(tasks) == CONCURRENCY:
@@ -40,10 +43,16 @@ async def send_requests():
 
 
 if __name__ == "__main__":
+    target = sys.argv[1]
+    numStrings = sys.argv[2]
+    stringLength = sys.argv[3]
+
+    URL = f"https://winter2023-comp598-group06-02.cs.mcgill.ca/{target}/sort/{numStrings}/{stringLength}/"
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop = asyncio.get_event_loop()
-    total_time, latencies = loop.run_until_complete(send_requests())
+    total_time, latencies = loop.run_until_complete(send_requests(URL))
     avg_throughput = NUM_REQUESTS / total_time
     avg_latency = sum(latencies) / NUM_REQUESTS
     print(f"Average throughput: {avg_throughput:.2f} requests/second")
